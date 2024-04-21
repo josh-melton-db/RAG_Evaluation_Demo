361b71f495000994dd9b0325d8bb61614f6fa882
josh-melton-db <josh-melton-db>
2024-04-21 21:03:49 +0000

    Removed old code, added example data

--- 0_setup.py
+++ 0_setup.py
@@ -7,23 +7,30 @@ text_col_name = "issue_description"
 text_id_name = "ticket_number"
 vector_search_endpoint_name = "one-env-shared-endpoint-5"
 
-from utils.demo import get_config, save_config, reset_tables, generate_source_data
+# Set to True and provide the domain and categories if you'd like to generate new data 
+generate_data_for_demo = True
+text_domain = "Field service maintenance tickets for a diesel engine manufacturer"
+category_ls = ["Overheated Turbocharger", "Fuel System Fault", "Worn Cylinder Head"]
 
+from utils.demo import get_config, save_config, reset_tables, generate_source_data
 config = get_config(catalog, schema, table, text_id_name, text_col_name, vector_search_endpoint_name)
 save_config(dbutils, config)
 
 # COMMAND ----------
 
-# TODO: set to default, if default use a csv in the repo, if not default generate the data
-# # Uncomment these lines to generate new data with your custom domain
-# # If you'd like, you can add categories for the domain.
-# # We will generate up to 10 categories for you
-# text_domain = "Field service maintenance tickets for a diesel engine manufacturer"
-# category_ls = ["Overheated Turbocharger", "Fuel System Fault", "Worn Cylinder Head"]
-# from langchain.chat_models import ChatDatabricks
-# chat_model = ChatDatabricks(endpoint="databricks-dbrx-instruct", max_tokens = 200)
-# reset_tables(spark, catalog, schema, config["demo_config"]["target_schema"])
-# generate_source_data(chat_model, text_domain, category_ls, text_col_name, text_id_name, catalog, schema, table, spark)
+# TODO: pull into demo.py
+if generate_data_for_demo:
+    from langchain.chat_models import ChatDatabricks
+    chat_model = ChatDatabricks(endpoint="databricks-dbrx-instruct", max_tokens = 200)
+    reset_tables(spark, catalog, schema, config["demo_config"]["target_schema"])
+    generate_source_data(chat_model, text_domain, category_ls, text_col_name, text_id_name, catalog, schema, table, spark)
+else:
+    dbx = dbutils.notebook.entry_point.getDbutils().notebook().getContext()
+    notebook_path = dbx.notebookPath().get()
+    folder_path = '/'.join(str(x) for x in notebook_path.split('/')[:-1])
+    import pandas as pd
+    df = spark.createDataFrame(pd.read_csv("/Workspace/"+folder_path+"/utils/example_data.csv"))
+    df.write.saveAsTable(f"{catalog}.{schema}.{table}")
 spark.read.table(f"{catalog}.{schema}.{table}").display()
 
 # COMMAND ----------
