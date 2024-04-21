90fed80e8dd2bb846268c691c02c2f9a15e3b26a
josh-melton-db <josh-melton-db>
2024-04-21 20:19:26 +0000

    Updated naming of generate source data function

--- 0_setup.py
+++ 0_setup.py
@@ -7,8 +7,7 @@ text_col_name = "issue_description"
 text_id_name = "ticket_number"
 vector_search_endpoint_name = "one-env-shared-endpoint-5"
 
-from utils.demo import get_config, save_config, reset_tables, generate_data
-from langchain.chat_models import ChatDatabricks
+from utils.demo import get_config, save_config, reset_tables, generate_source_data
 
 config = get_config(catalog, schema, table, text_id_name, text_col_name, vector_search_endpoint_name)
 save_config(dbutils, config)
@@ -21,9 +20,10 @@ save_config(dbutils, config)
 # # We will generate up to 10 categories for you
 # text_domain = "Field service maintenance tickets for a diesel engine manufacturer"
 # category_ls = ["Overheated Turbocharger", "Fuel System Fault", "Worn Cylinder Head"]
+# from langchain.chat_models import ChatDatabricks
 # chat_model = ChatDatabricks(endpoint="databricks-dbrx-instruct", max_tokens = 200)
 # reset_tables(spark, catalog, schema, config["demo_config"]["target_schema"])
-# generate_data(chat_model, text_domain, category_ls, text_col_name, text_id_name, catalog, schema, table, spark)
+# generate_source_data(chat_model, text_domain, category_ls, text_col_name, text_id_name, catalog, schema, table, spark)
 spark.read.table(f"{catalog}.{schema}.{table}").display()
 
 # COMMAND ----------
