76dff95862471aec59853702c22b7fb9a981c5f3
josh-melton-db <josh-melton-db>
2024-04-24 20:40:10 +0000

    Added DSPy example and gitignore for DSPy logs

---
+++ 5_prompt_tuning_dspy.py
@@ -0,0 +1,90 @@
+# Databricks notebook source
+# MAGIC %pip install dspy-ai --upgrade -q
+# MAGIC dbutils.library.restartPython()
+
+# COMMAND ----------
+
+import dspy
+
+token = dbutils.notebook.entry_point.getDbutils().notebook().getContext().apiToken().get()
+url = dbutils.notebook.entry_point.getDbutils().notebook().getContext().apiUrl().get() + '/serving-endpoints'
+
+# Set up the LM
+dbrx = dspy.Databricks(model='databricks-dbrx-instruct', model_type='chat', api_key=token, api_base=url, max_tokens=1000)
+dspy.settings.configure(lm=dbrx)
+
+# COMMAND ----------
+
+class CoT(dspy.Module):
+    """Generates a response to the request using retrieved input for grounding"""
+    def __init__(self):
+        super().__init__()
+        self.program = dspy.ChainOfThought("request, context -> response")
+    
+    def forward(self, request, context):
+        return self.program(request=request, context=context)
+
+# COMMAND ----------
+
+golden_dataset = spark.sql("""
+    SELECT request, response,
+        CONCAT_WS('\nContext: ', transform(eval.retrieval_context, x -> x.content)) as context
+    FROM `default`.`generated_rag_demo`.`synthetic_eval_set_eval_metrics` eval
+    WHERE eval.response_metrics.llm_judged_relevant_to_question_and_context = 1
+""").toPandas()
+
+trainset = [dspy.Example(request=row['request'], context=row['context'], response=row['response']).with_inputs('request', 'context')
+           for i, row in golden_dataset.iterrows()]
+
+# COMMAND ----------
+
+class AssessResponse(dspy.Signature):
+    """Assess the quality of an outline along the specified dimension."""
+    response_to_assess = dspy.InputField()
+    assessment_question = dspy.InputField()
+    assessment_answer = dspy.OutputField(desc="Yes or No")
+
+# COMMAND ----------
+
+def metric(gold, pred, trace=None):
+    request, context, response, generated = gold.request, gold.context, gold.response, pred.response
+    harmless_q = "Is the response harmless and unoffensive to the user?"
+    specific_q = "Does the response provide very detailed, specific actions to take to identify or resolve the request?"
+    value_add_q = "Does the response avoid simply repeating back the provided request and add value to the conversation?"
+    harmless_eval =  dspy.Predict(AssessResponse)(response_to_assess=generated, assessment_question=harmless_q)
+    specific_eval =  dspy.Predict(AssessResponse)(response_to_assess=generated, assessment_question=specific_q)
+    value_add_eval = dspy.Predict(AssessResponse)(response_to_assess=generated, assessment_question=value_add_q)
+    evals = [m.assessment_answer.lower() == 'yes' for m in [harmless_eval, specific_eval, value_add_eval]]
+    score = sum(evals)
+
+    if trace is not None: return score >= 2
+    return score >= 1
+
+# COMMAND ----------
+
+from dspy.teleprompt import BootstrapFewShot
+
+# Set up the optimizer: we want to "bootstrap" (i.e., self-generate) 4-shot examples of our CoT program.
+config = dict(max_bootstrapped_demos=1, max_labeled_demos=2, max_errors=1)
+
+# Optimize! Use the custom here. In general, the metric is going to tell the optimizer how well it's doing.
+teleprompter = BootstrapFewShot(metric=metric, **config)
+optimized_cot = teleprompter.compile(CoT(), trainset=trainset)
+
+# COMMAND ----------
+
+from dspy.evaluate import Evaluate
+
+# Set up the evaluator, which can be used multiple times.
+evaluate = Evaluate(devset=trainset, metric=metric, num_threads=4, display_progress=True, display_table=0)
+
+# Evaluate our `optimized_cot` program.
+evaluate(optimized_cot)
+
+# COMMAND ----------
+
+dbrx.inspect_history(n=3)
+
+# COMMAND ----------
+
+
